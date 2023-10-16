7a80cfb55ab7ed53f697c8f4cd2fe20e0ee534c7
Ebrahim Reda <119238955+ebrahimabdallah@users.noreply.github.com>
2023-10-17 00:30:33 +0300

    Update Donation_Request.php

--- app/Models/Donation_Request.php
+++ app/Models/Donation_Request.php
@@ -12,14 +12,6 @@ class Donation_Request extends Model
     protected $fillable = array('patient_name', 'patients_phone', 
     'hospital_name', 'bloode_type', 'patient_age', 'bages_num','address');
 
-    public function City()
-    {
-        return $this->belongsTo('App\Models\City');
-    }
-
-    public function Clients()
-    {
-        return $this->belongsToMany('App\Models\Clients');
-    }
+   
 
 }
