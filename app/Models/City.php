b280176e754c48858a780e72a4b5eb24513df349
Ebrahim Reda <119238955+ebrahimabdallah@users.noreply.github.com>
2023-10-17 00:30:19 +0300

    Update City.php

--- app/Models/City.php
+++ app/Models/City.php
@@ -10,20 +10,8 @@ class City extends Model
     protected $table = 'cities';
     public $timestamps = true;
     protected $fillable = array('name', 'governorate_id');
-
-    public function clients()
-    {
-        return $this->hasMany('App/Model\Clients');
-    }
-
-    public function Governorate()
-    {
-        return $this->belongsTo('App/Models\Governorate');
-    }
-
-    public function cities()
-    {
-        return $this->hasMany('App/Models\Donation_Request');
-    }
+ 
+ 
+ 
 
 }
